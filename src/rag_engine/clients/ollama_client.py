a7d648d095d3a7e163076ee3b1939178a78c10bd
Zaido219 <jpmalbas4@gmail.com>
2026-08-23 22:21:21 +0800

    feat: WIP implementing the embedding model

--- src/rag_engine/clients/ollama_client.py
+++ src/rag_engine/clients/ollama_client.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import ollama
 
 class BaseEmbeddingModel(ABC):
     @abstractmethod
@@ -7,3 +8,19 @@ class BaseEmbeddingModel(ABC):
     @abstractmethod
     def embed_documents(self, texts:list[str]) -> list[list[float]]:
         pass
+
+
+class OllamaEmbeddingModel(BaseEmbeddingModel):
+    def __init__(self, model_name:str="nomic-embed-text", host:str="http://localhost:11434"):
+        super().__init__()
+        self.model_name = model_name
+        self.client = ollama.Client(host=host)
+
+    def embed_query(self, text:str) -> list[float]:
+        response = self.client.embed(model=self.model_name, input=text)
+        return response["embeddings"][0] #this will be a list of vectors we will only be returning the first one
+
+    def embed_documents(self, texts:list[str]) -> list[list[float]]:
+        # Take advantage of Ollama's native batch embedding support
+        response = self.client.embed(model=self.model_name, input=texts)
+        return response["embeddings"]
