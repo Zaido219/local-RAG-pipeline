7fa39159469bea7f944e9f55b2d76190504249ee
Zaido219 <jpmalbas4@gmail.com>
2026-08-26 00:16:07 +0800

    feat: better chunking

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from rag_engine.services.document_processor import DocumentProcessor
 from rag_engine.services.vector_repository import ChromaVectorRepository
 from rag_engine.clients.ollama_client import OllamaEmbeddingModel
 from rag_engine.services.pipeline import RAGPipeline
+from rag_engine.services.retriever import RetrieverService
+from rag_engine.services.prompt_builder import PromptBuilderService
+from rag_engine.clients.ollama_client import GeminiInferenceClient, OllamaEmbeddingModel
 
 def main():
     chroma_path = os.getenv("CHROMA_PERSIST_DIR", "./chroma_db")
@@ -15,11 +18,17 @@ def main():
         collection_name="constitution_docs"
     )
     embedder = OllamaEmbeddingModel(model_name="nomic-embed-text", host=ollama_url)
+    retriever = RetrieverService(vector_repo=vector_repo, embedding_model=embedder)
+    prompt_builder = PromptBuilderService()
+    inference_client = GeminiInferenceClient()
 
     pipeline = RAGPipeline(
         document_processor=processor,
         vector_repo=vector_repo,
         embedding_model=embedder,
+        retriever=retriever,
+        prompt_builder=prompt_builder,
+        inference_client=inference_client
     )
 
     document_path = "sample.txt"
