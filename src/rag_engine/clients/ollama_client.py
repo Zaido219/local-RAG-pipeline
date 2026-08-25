ed0318b93f777e5629d91200fe94a1b743b7c690
Zaido219 <jpmalbas4@gmail.com>
2026-08-25 22:34:44 +0800

    feat: WIP integrating to external gemini api

--- src/rag_engine/clients/ollama_client.py
+++ src/rag_engine/clients/ollama_client.py
@@ -1,5 +1,6 @@
 from rag_engine.core.interfaces import BaseEmbeddingModel, BaseInferenceClient
 import ollama
+import os
 
 
 class OllamaEmbeddingModel(BaseEmbeddingModel):
@@ -38,3 +39,22 @@ class OllamaInferenceClient(BaseInferenceClient):
                 "keep_alive": 0
                 })
         return response["response"]
+
+
+class GeminiInferenceClient(BaseInferenceClient):
+    def __init__(self, model_name:str="gemini-2.5-flash", key:str | None = None):
+        self.model_name = model_name
+        api_key = key or os.getenv("API_KEY")
+
+        if not api_key:
+            raise ValueError("Missing api key")
+        # initialize the google genai client
+        self.client = genai.Client(api_key=key)
+
+    def generate(self, prompt:str):
+        response = self.client.models.generate_content(
+            model = self.model_name,
+            contents = prompt,
+        )
+
+        return response.text
